dfc287d4dc209777aba6ea0100099d3a70ca9dee
Lucas Lacerda <lucaslacerdajob@gmail.com>
2022-10-30 01:05:09 -0300

    Initial Commit

---
+++ LucasCard/LucasCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LucasCardApp.swift
+//  LucasCard
+//
+//  Created by Lucas Lacerda on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LucasCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
